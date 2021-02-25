1a3f5e1497e60d52b60264bb036081125fadccad
pewinski <pewinski@pdtec.de>
2021-02-25 18:07:35 +0100

    installed locally

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +6,19 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+  displayedColumns = ["name", "position", "weight", "symbol", "star"];
+  dataSource = ELEMENT_DATA;
 }
+
+const ELEMENT_DATA: any[] = [
+  { numberPre: 1, numberSuf: 1, item: "Hydrogen", id: 10079, color: "H" , size: 20},
+  { numberPre: 2, numberSuf: 2, item: "Helium", id: 40026, color: "He", size: 20 },
+  { numberPre: 3, numberSuf: 3, item: "Lithium", id: 6941, color: "Li", size: 20 },
+  { numberPre: 4, numberSuf: 4, item: "Beryllium", id: 90122, color: "Be", size: 20 },
+  { numberPre: 5, numberSuf: 5, item: "Boron", id: 10811, color: "B" , size: 20},
+  { numberPre: 6, numberSuf: 6, item: "Carbon", id: 120107, color: "C" , size: 20},
+  { numberPre: 7, numberSuf: 7, item: "Nitrogen", id: 140067, color: "N" , size: 20},
+  { numberPre: 8, numberSuf: 8, item: "Oxygen", id: 159994, color: "O" , size: 20},
+  { numberPre: 9, numberSuf: 9, item: "Fluorine", id: 189984, color: "F" , size: 20},
+  { numberPre: 10, numberSuf: 10, item: "Neon", id: 201797, color: "Ne", size: 20 }
+];
