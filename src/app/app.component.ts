98729bad49b703818a68d4aaca970d779d9f2284
pewinski <pewinski@pdtec.de>
2021-02-25 18:11:50 +0100

    add material dependency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  displayedColumns = ["name", "position", "weight", "symbol", "star"];
+  displayedColumns = ["numberPre", "numberSuf", "item", "id", "color", "size"];
   dataSource = ELEMENT_DATA;
 }
 
 const ELEMENT_DATA: any[] = [
-  { numberPre: 1, numberSuf: 1, item: "Hydrogen", id: 10079, color: "H" , size: 20},
-  { numberPre: 2, numberSuf: 2, item: "Helium", id: 40026, color: "He", size: 20 },
-  { numberPre: 3, numberSuf: 3, item: "Lithium", id: 6941, color: "Li", size: 20 },
-  { numberPre: 4, numberSuf: 4, item: "Beryllium", id: 90122, color: "Be", size: 20 },
-  { numberPre: 5, numberSuf: 5, item: "Boron", id: 10811, color: "B" , size: 20},
-  { numberPre: 6, numberSuf: 6, item: "Carbon", id: 120107, color: "C" , size: 20},
-  { numberPre: 7, numberSuf: 7, item: "Nitrogen", id: 140067, color: "N" , size: 20},
-  { numberPre: 8, numberSuf: 8, item: "Oxygen", id: 159994, color: "O" , size: 20},
-  { numberPre: 9, numberSuf: 9, item: "Fluorine", id: 189984, color: "F" , size: 20},
-  { numberPre: 10, numberSuf: 10, item: "Neon", id: 201797, color: "Ne", size: 20 }
+  { numberPre: 1, numberSuf: 1, item: "Hydrogen", id: "100791007910079100791007910079100791007910079", color: "H" , size: 20},
+  { numberPre: 2, numberSuf: 2, item: "Helium", id: "400264002640026400264002640026400264002640026", color: "He", size: 20 },
+  { numberPre: 3, numberSuf: 3, item: "Lithium", id: "694169416941694169416941694169416941", color: "Li", size: 20 },
+  { numberPre: 4, numberSuf: 4, item: "Beryllium", id: "901229012290122901229012290122901229012290122", color: "Be", size: 20 },
+  { numberPre: 5, numberSuf: 5, item: "Boron", id: "108111081110811108111081110811108111081110811", color: "B" , size: 20},
+  { numberPre: 6, numberSuf: 6, item: "Carbon", id: "120107120107120107120107120107120107120107120107120107", color: "C" , size: 20},
+  { numberPre: 7, numberSuf: 7, item: "Nitrogen", id: "140067140067140067140067140067140067140067140067140067", color: "N" , size: 20},
+  { numberPre: 8, numberSuf: 8, item: "Oxygen", id: "159994159994159994159994159994159994159994159994159994", color: "O" , size: 20},
+  { numberPre: 9, numberSuf: 9, item: "Fluorine", id: "189984189984189984189984189984189984189984189984189984", color: "F" , size: 20},
+  { numberPre: 10, numberSuf: 10, item: "Neon", id: "201797201797201797201797201797201797201797201797201797", color: "Ne", size: 20 }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { MatTableModule } from '@angular/material/table';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, MatTableModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
